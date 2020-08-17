c59bec926f2c7b770afb052e0573cad2d8d46955
LeandroBonifacio <69816625+LeandroBonifacio@users.noreply.github.com>
2020-08-17 16:05:45 -0300

    Add files via upload

---
+++ Js/getData.js
@@ -0,0 +1,8 @@
+var getData = function () {
+    var Nombre = document.getElementById('nombre'.value;
+    var Correo = document.getElementById('correo').value;
+    var Telefono = document.getElementById('telefono').value;
+    var Mensaje = document.getElementById('mensaje').value;
+    
+    alert(Nombre + " " +Correo + " " + Telefono + " " + Mensaje + " ");
+}
